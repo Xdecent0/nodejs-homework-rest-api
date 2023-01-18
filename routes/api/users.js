81f6086c69dba90696b3aa50117bb4cb4da1bf17
Xdecent <xdecentwork@gmail.com>
2023-01-18 19:54:04 +0200

    done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { auth, ctrlWrapper } = require("../../middlewars/index.js");
-const { getCurrent } = require("../../controllers/users/index.js");
+const { auth, upload, ctrlWrapper } = require("../../middlewars/index.js");
+const {
+  getCurrent,
+  updateAvatar,
+} = require("../../controllers/users/index.js");
 
 const router = express.Router();
 
 router.get("/current", auth, ctrlWrapper(getCurrent));
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  ctrlWrapper(updateAvatar)
+);
 
 module.exports = router;
